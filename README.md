ac7fc9fd1388e5e819ba5ed87f0609509343a20a
1303575952 <1303575952@qq.com>
2018-10-30 11:44:25 +0800

    Longest Palindromic Substring

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 * 1.[Two Sum] (easy) *2018/10/29 finished*
 * 2.[Add Two Numbers] (Medium) *20181029 finished*
 * 3.[Longest Substring Without Repeating Characters] (Medium) *20181030 finished*
-* 4.[Median of Two Sorted Arrays] (Hard)
+* 4.[Median of Two Sorted Arrays] (Hard) **NOT UNDERSTAND**
+* 5.[Longest Palindromic Substring] (Medium) *2018/10/30 finished* **There is even an O(n) algorithm called Manacher's algorithm**
---
+++ src/topics/dynamicprogramming/Q5LongestPalindromicSubstring.java
@@ -0,0 +1,49 @@
+package topics.dynamicprogramming;
+
+/**
+ * 
+ * @author FELIX
+ *
+ *         Given a string s, find the longest palindromic substring in s. You
+ *         may assume that the maximum length of s is 1000.
+ * 
+ *         Example 1: Input: "babad" Output: "bab" Note: "aba" is also a valid answer.
+ * 
+ *         Example 2: Input: "cbbd" Output: "bb"
+ */
+public class Q5LongestPalindromicSubstring {
+	public static void main(String[] args) {
+		String str = "babad";
+		System.out.println(new Q5LongestPalindromicSubstring().getLongestPalindromicSubString(str));
+	}
+	public String getLongestPalindromicSubString(String str){
+		StringBuffer sb = new StringBuffer("");
+		sb.append("#");
+		for(int i=0;i<str.length();i++){
+			sb.append(str.charAt(i));
+			sb.append("#");
+		}
+		String newStr = sb.toString();
+		String longestSubString = "";
+		
+		for(int i=1;i<newStr.length()/2+1;i++){
+			for(int j=0;i-j>=0;){
+				if(newStr.charAt(i-j)==newStr.charAt(i+j)){
+					if((2*j+1)>longestSubString.length()){
+						longestSubString = newStr.substring(i-j, i+j+1);
+					}
+					j++;
+				}else{
+					break;
+				}
+			}
+		}
+		StringBuffer sbAns = new StringBuffer("");
+		for(int i=0;i<longestSubString.length();i++){
+			if(newStr.charAt(i)!='#'){
+				sbAns.append(longestSubString.charAt(i));
+			}
+		}
+		return sbAns.toString();
+	}
+}
