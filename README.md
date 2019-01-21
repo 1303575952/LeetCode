c8b19235dc8c0df688c54431c3391eaa39b9eb8a
1303575952@qq.com <1303575952@qq.com>
2019-01-21 16:46:30 +0800

    完善Q1_TwoSum

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # LeetCode-lifei
 重做一遍LeetCode
 
-* 1.[Two Sum] (easy) *20181029 finished*
+* 1.[Two Sum] (Array & Hash Table) (easy) *20181029 finished*
 * 2.[Add Two Numbers] (Medium) *20181029 finished*
 * 3.[Longest Substring Without Repeating Characters] (Medium) *20181030 finished*
 * 4.[Median of Two Sorted Arrays] (Hard) **NOT UNDERSTAND**
--- src/topics/hashtable/Q1_TwoSum.java
+++ src/topics/hashtable/Q1_TwoSum.java
@@ -1,41 +1,43 @@
 package topics.hashtable;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 
  * @author FELIX
- * 
- *         Question:Given an array of integers, return indices of the two
- *         numbers such that they add up to a specific target. You may assume
- *         that each input would have exactly one solution, and you may not use
- *         the same element twice.
- * 
- *         Example: Given nums = [2, 7, 11, 15], target = 9, Because nums[0] +
- *         nums[1] = 2 + 7 = 9, return [0, 1].
+ * <p>
+ * Question:Given an array of integers, return indices of the two
+ * numbers such that they add up to a specific target. You may assume
+ * that each input would have exactly one solution, and you may not use
+ * the same element twice.
+ * <p>
+ * Example: Given nums = [2, 7, 11, 15], target = 9, Because nums[0] +
+ * nums[1] = 2 + 7 = 9, return [0, 1].
  */
 public class Q1_TwoSum {
-	public static void main(String[] args) {
-		int[] arr = { 2, 7, 11, 15 };
-		int target = 9;
-		int[] ans = new Q1_TwoSum().getIndexOfTwoNums(arr, target);
-		System.out.println(ans[0] + " " + ans[1]);
-	}
+    public static void main(String[] args) {
+        int[] arr = {3, 3};
+        int target = 6;
+        int[] ans = new Q1_TwoSum().twoSum(arr, target);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
 
-	public int[] getIndexOfTwoNums(int[] arr, int target) {
-		int[] ans = new int[2];
-		HashMap<Integer, Integer> hashmap = new HashMap<Integer, Integer>();
-		for (int i = 0; i < arr.length; i++) {
-			hashmap.put(arr[i], i);
-		}
-		for (int i = 0; i < arr.length; i++) {
-			int key = target - arr[i];
-			if (hashmap.get(key) != null) {
-				ans[0] = i;
-				ans[1] = hashmap.get(key);
-				break;
-			}
-		}
-		return ans;
-	}
+    public int[] twoSum(int[] arr, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], i);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int another = target - arr[i];
+            if (map.containsKey(another)) {
+                ans[0] = i;
+                ans[1] = map.get(another);
+                if (ans[0] != ans[1]) {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
 }
